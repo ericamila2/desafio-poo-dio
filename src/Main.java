2329dbda81d1ef1680838bc6d40bb4057dbdbb62
Erica Camila <108944029+ericamila@users.noreply.github.com>
2024-08-19 09:44:56 -0300

    Up de fork

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,18 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
-        curso1.setTitulo("curso java");
-        curso1.setDescricao("descrição curso java");
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descrição curso java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso2.setTitulo("curso js");
-        curso2.setDescricao("descrição curso js");
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JS");
         curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("mentoria de java");
-        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
         /*System.out.println(curso1);
@@ -33,16 +33,16 @@ public class Main {
         bootcamp.getConteudos().add(curso2);
         bootcamp.getConteudos().add(mentoria);
 
-        Dev devCamila = new Dev();
-        devCamila.setNome("Camila");
-        devCamila.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
-        devCamila.progredir();
-        devCamila.progredir();
+        Dev devErica = new Dev();
+        devErica.setNome("Erica");
+        devErica.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Erica:" + devErica.getConteudosInscritos());
+        devErica.progredir();
+        devErica.progredir();
         System.out.println("-");
-        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
-        System.out.println("Conteúdos Concluídos Camila:" + devCamila.getConteudosConcluidos());
-        System.out.println("XP:" + devCamila.calcularTotalXp());
+        System.out.println("Conteúdos Inscritos Erica:" + devErica.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Erica:" + devErica.getConteudosConcluidos());
+        System.out.println("XP:" + devErica.calcularTotalXp());
 
         System.out.println("-------");
 
